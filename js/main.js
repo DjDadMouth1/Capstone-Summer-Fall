dd62d0241e808889a7285ab569349bc3c7723a5d
Tuan Nguyen <nguyenvutuan1@gmail.com>
2021-10-18 18:40:02 -0700

    only display download button when downloadFile name exist

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 
 //const downloadFile = "./sample.csv"
 const downloadFile = "./result.txt"
+//const downloadFile = "";
 
 $(".run-frictionless").click(function (e) { 
     console.log("runing!!!")
-    $(".download").append(`
-    <a href="${downloadFile}" download>
-        <div class="download-btn">
-            Download File
-            <i class="fas fa-download"></i>
-        </div>
-    </a>
-    `);
+    if(downloadFile){
+        $(".download").append(`
+        <a href="${downloadFile}" download>
+            <div class="download-btn">
+                Download File
+                <i class="fas fa-download"></i>
+            </div>
+        </a>
+        `);
+    }
+    
 });
