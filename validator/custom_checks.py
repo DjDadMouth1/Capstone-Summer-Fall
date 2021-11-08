00fcaa906bd2f147eb36aa6cc5122236efd21c9b
Jordan Malubay <jmalubay@pdx.edu>
2021-11-07 18:14:10 -0800

    Removed Duplicate check

--- validator/custom_checks.py
+++ validator/custom_checks.py
@@ -613,30 +613,6 @@ class bureau_code_match_error(Check):
     }
 
     # FAIHAN
-class duplicate_row(Check):
-    code = "duplicate-row"
-    Errors = [errors.DuplicateRowError]
-
-    def __init__(self, descriptor=None):
-        super().__init__(descriptor)
-        self.__memory = {}
-
-    def validate_row(self, row):
-        text = ",".join(map(str, row.values()))
-        hash = hashlib.sha256(text.encode("utf-8")).hexdigest()
-        match = self.__memory.get(hash)
-        if match:
-            note = 'duplicate-row the same as row at position "%s"' % match
-            yield errors.DuplicateRowError.from_row(row, note=note)
-        self.__memory[hash] = row.row_position
-
-    # Metadata
-    metadata_profile = {  # type: ignore
-        "type": "object",
-        "properties": {},
-    }
-
-
 class valid_Email_In_Cell(Check):
     code = "cell-error"
     Errors = [errors.CellError]
