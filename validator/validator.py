13461c889361aef0d0cbb150eb448e55dccfd978
Jordan Malubay <jmalubay@pdx.edu>
2021-11-08 20:05:35 -0800

    Slight flask validate fix

--- validator/validator.py
+++ validator/validator.py
@@ -3,10 +3,10 @@ from frictionless import validate
 import json
 
 def custom_validate(file):
-    checks = [custom_checks.numeric_field_error()]
-    report = validate(file, checks)
+    new_checks = [custom_checks.numeric_field()]
+    report = validate(file, checks=new_checks)
     with open('report.json','w') as outfile:
-        json.dump(report.flatten(['message']),outfile, indent=2)
+        json.dump(report.flatten(),outfile, indent=2)
     
     return 
 
