c0a7a8744cb6d9a7aa1c970b5db5c1efa62f6ef8
Jordan Malubay <jmalubay@pdx.edu>
2021-11-17 19:48:04 -0800

    added main to app.py

--- app.py
+++ app.py
@@ -15,5 +15,6 @@ def run_validator():
    from validator import validator
    validator.custom_validate(filename)
    return send_file('report.json')
-   
-app.run(host='localhost', port=5000)
+
+if __name__ == "__main__":
+   app.run(host='localhost', port=5000)
