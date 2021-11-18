98762a92855c56773b72fc1a6928026feaea0258
Jordan Malubay <jmalubay@pdx.edu>
2021-11-17 20:10:44 -0800

    Added selection to validator

--- validator/validator.py
+++ validator/validator.py
@@ -1,12 +1,21 @@
 from . import custom_checks
-from frictionless import validate
+from frictionless import describe, validate
 import json
 
-def custom_validate(file):
-    new_checks = [custom_checks.numeric_field()]
+def custom_validate(file, output_selection = None):
+    output_report = None
+    new_checks = [custom_checks.monetary_fields()]
     report = validate(file, checks=new_checks)
+    
+    if output_selection == 'schema':
+        output_report = describe(file)
+    elif output_selection == 'error':
+        output_report = report.flatten(['message'])
+    else:
+        output_report = report
+
     with open('report.json','w') as outfile:
-        json.dump(report.flatten(),outfile, indent=2)
+        json.dump(output_report,outfile, indent=2)
     
     return 
 
